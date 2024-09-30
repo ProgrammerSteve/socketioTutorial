55ff6ca58513353155d660fe91b8f1801b3f5a3f
Steven <stevenmb1995@gmail.com>
2024-09-29 23:10:11 -0500

    got to the last step from the socket.io tutorial

--- index.js
+++ index.js
@@ -1,32 +1,95 @@
 import express from "express";
-import {createServer} from "http"
-import {fileURLToPath} from "url"
-import {dirname,join} from "path"
-import {Server} from "socket.io"
+import { createServer } from "http";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import { availableParallelism } from "os";
+import cluster from "cluster";
+import { createAdapter, setupPrimary } from "@socket.io/cluster-adapter";
 
-const app=express()
-const server=createServer(app)
-const io= new Server(server)
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i,
+    });
+  }
+  setupPrimary();
+} else {
+  const db = await open({
+    filename: "chat.db",
+    driver: sqlite3.Database,
+  });
 
-const __dirname=dirname(fileURLToPath(import.meta.url))
+  await db.exec(`
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            client_offset TEXT UNIQUE,
+            content TEXT
+        );
+    `);
 
-app.get("/",(req,res)=>{
-    res.sendFile(join(__dirname,'index.html'));
-});
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+    ackTimeout: 10000,
+    retries: 3,
+    adapter: createAdapter(),
+  });
 
-io.on('connection', (socket)=>{
-    socket.broadcast.emit('hi');
+  const __dirname = dirname(fileURLToPath(import.meta.url));
 
-    console.log("a user connected")
-    socket.on('disconnect',()=>{
-        console.log('user disconnected')
-    })
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-      });
-})
+  app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "index.html"));
+  });
 
-server.listen(3000,()=>{
-    console.log(`server running at http://localhost:3000`)
-})
+  io.on("connection", async (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+
+    socket.on("chat message", async (msg, clientOffset, callback) => {
+      let result;
+      try {
+        result = await db.run(
+          "INSERT INTO messages (content, client_offset) VALUES (?, ?)",
+          msg,
+          clientOffset
+        );
+      } catch (e) {
+        if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
+          callback();
+        } else {
+          // nothing to do, just let the client retry
+        }
+        return;
+      }
+      io.emit("chat message", msg, result.lastID);
+      callback();
+    });
+
+    if (!socket.recovered) {
+      try {
+        await db.each(
+          "SELECT id, content FROM messages WHERE id > ?",
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit("chat message", row.content, row.id);
+          }
+        );
+      } catch (e) {
+        // something went wrong
+      }
+    }
+  });
+
+  const port = process.env.PORT;
+
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+}
